89a423395e3c9109fd0552be741f12856b3219df
Mustafa <mustafa.raja.here@gmail.com>
2024-10-03 00:25:47 +0500

    native wind setup with SDK 50

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center bg-red-400"
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
