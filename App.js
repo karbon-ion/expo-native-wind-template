2096fd3f9a04bde1738a715c4ecb0f04c5a86fd3
Hasham Asad <tipuasad2001@gmail.com>
2024-12-10 20:44:57 +0500

    'nativiceWind52'

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 import ResetPasswordScreen from './src/screens/ResetPasswordScreen';
+import "./global.css"
 
 const Stack = createNativeStackNavigator();
 
